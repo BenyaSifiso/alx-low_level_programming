261eb659ff973da14b45f1fd710434996cac04bd
BenyaSifiso <benyasifiso@gmail.com>
2023-05-12 18:56:28 -0700

    update

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,7 +11,7 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-int o, w, len = 0;
+int z, w, len = 0;
 if (filename == NULL)
 return (-1);
 if (text_content != NULL)
@@ -19,10 +19,10 @@ if (text_content != NULL)
 for (len = 0; text_content[len];)
 len++;
 }
-w = write(o, text_content, len);
-o = open(filename, O_WRONLY | O_APPEND);
-if (o == -1 || w == -1)
+w = write(z, text_content, len);
+z = open(filename, O_WRONLY | O_APPEND);
+if (z == -1 || w == -1)
 return (-1);
-close(o);
+close(z);
 return (1);
 }
